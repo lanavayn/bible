9946b24725b628b8c602f800aa2f38a8532d8720
Lana Vayner <lanazvorygin@gmail.com>
2026-03-20 13:55:54 -0400

    Added install app

---
+++ js/sw.js
@@ -0,0 +1,8 @@
+// /js/sw.js
+self.addEventListener('install', event => {
+    self.skipWaiting();
+  });
+  
+  self.addEventListener('fetch', event => {
+    event.respondWith(fetch(event.request));
+  });
