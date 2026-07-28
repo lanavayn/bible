145f239d5bea48c4a291bae63f71cb04c7ce60e7
Lana Vayner <lanazvorygin@gmail.com>
2026-07-27 22:15:42 -0400

    Confirm new OneSignal subscription opt-in

--- js/onesignal-notifications.js
+++ js/onesignal-notifications.js
@@ -149,11 +149,7 @@ function initNotificationControls(root = document, feature) {
         return;
       }
 
-      if (OneSignal.User?.PushSubscription?.optIn) {
-        await OneSignal.User.PushSubscription.optIn();
-      }
-
-      const subscription = await waitForActivePushSubscription(OneSignal);
+      const subscription = await ensurePushSubscriptionOptedIn(OneSignal);
       const tags = await tagNotificationSubscriber(OneSignal, feature, language, true);
 
       await logNotificationDiagnostics(OneSignal, "after-enable", { feature, language, tags });
@@ -341,6 +337,9 @@ async function ensurePushSubscriptionOptedIn(OneSignal) {
   if (OneSignal.User?.PushSubscription?.optIn) {
     try {
       await OneSignal.User.PushSubscription.optIn();
+      const subscription = await waitForActivePushSubscription(OneSignal);
+      await OneSignal.User.PushSubscription.optIn();
+      return subscription;
     } catch (error) {
       const subscriptionAfterError = getPushSubscriptionState(OneSignal);
 
